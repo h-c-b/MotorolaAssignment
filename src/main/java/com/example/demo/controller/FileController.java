0744139fa98de0242958007eeeaf877ad079f2ed
h-c-b <hanna.baekkevold@gmail.com>
2024-03-11 00:55:00 +0000

    Return file to download or return correct status code with error message

--- src/main/java/com/example/demo/controller/FileController.java
+++ src/main/java/com/example/demo/controller/FileController.java
@@ -1,8 +1,15 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.FileService;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
 
 @RestController
 @RequestMapping("/api/files")
@@ -22,6 +29,20 @@ public class FileController {
      }
 
      @GetMapping("/{fileName}")
-     public void downloadFileByName(@RequestParam String fileName) {
+     public ResponseEntity<?> downloadFileByName(@PathVariable String fileName) {
+        if(fileName.contains(" ")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("");
+        }
+        try {
+           Resource fileToDownLoad  = _fileService.downloadFile(fileName);
+           return ResponseEntity.status(HttpStatus.FOUND)
+                   .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                   .contentType(MediaType.parseMediaType("application/octet-stream"))
+                   .body(fileToDownLoad);
+        } catch(FileNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("");
+        } catch(Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
+        }
      }
 }
