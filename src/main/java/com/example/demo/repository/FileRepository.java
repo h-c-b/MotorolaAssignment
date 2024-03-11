400d246caa7be4e43b131c58295c53841ab17d17
h-c-b <hanna.baekkevold@gmail.com>
2024-03-11 00:48:05 +0000

    Implement method to get a file by name in the FileRepository

--- src/main/java/com/example/demo/repository/FileRepository.java
+++ src/main/java/com/example/demo/repository/FileRepository.java
@@ -1,12 +1,13 @@
 package com.example.demo.repository;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
 
 @Repository
 public class FileRepository {
@@ -14,8 +15,8 @@ public class FileRepository {
     private final String _rootDir = "C:/Users/hanna/fileFolder";
     public void writeFile(MultipartFile file) {
         try {
-            Files.createDirectories(Paths.get(_rootDir));
-            var path = Paths.get(_rootDir, file.getOriginalFilename());
+            Files.createDirectories(Path.of(_rootDir));
+            var path = Path.of(_rootDir, file.getOriginalFilename());
             Files.copy(file.getInputStream(), path);
         } catch (FileAlreadyExistsException e) {
             throw new RuntimeException();
@@ -23,11 +24,17 @@ public class FileRepository {
         catch (IOException e) {
             throw new RuntimeException(e);
         }
-    };
+    }
 
-    public void getFile(String name) {
-    };
+    public Resource getFile(String fileName) {
+        var path = Path.of(_rootDir, fileName);
+        try {
+            return new UrlResource(path.toUri());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-    public void getFileNames() {};
+    public void getFileNames() {}
 
 }
