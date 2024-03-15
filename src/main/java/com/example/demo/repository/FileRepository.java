8548169e49d42a14359e096f931a2eef287ba2a9
h-c-b <hanna.baekkevold@gmail.com>
2024-03-15 05:41:40 +0000

    Remove exceptionhandling from repository

--- src/main/java/com/example/demo/repository/FileRepository.java
+++ src/main/java/com/example/demo/repository/FileRepository.java
@@ -1,40 +1,33 @@
 package com.example.demo.repository;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 @Repository
-public class FileRepository {
+public class FileRepository implements IFileRepository {
 
-    private final String _rootDir = "C:/Users/hanna/fileFolder";
-    public void writeFile(MultipartFile file) {
-        try {
-            Files.createDirectories(Path.of(_rootDir));
-            var path = Path.of(_rootDir, file.getOriginalFilename());
-            Files.copy(file.getInputStream(), path);
-        } catch (FileAlreadyExistsException e) {
-            throw new RuntimeException();
-        }
-        catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    @Value("${fileStoreRoot}")
+    private String rootDirPath;
+    
+    public boolean saveFile(MultipartFile file) throws IOException {
+        Files.createDirectories(Path.of(rootDirPath));
+        return Files.copy(file.getInputStream(), Path.of(rootDirPath, file.getOriginalFilename())) > 0;
     }
 
-    public Resource getFile(String fileName) {
-        var path = Path.of(_rootDir, fileName);
-        try {
-            return new UrlResource(path.toUri());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public Resource getFile(String fileName) throws MalformedURLException {
+        return new UrlResource(Path.of(rootDirPath, fileName).toUri());
     }
 
-    public void getFileNames() {}
-
+    public Stream<Path> getFileNames() throws IOException {
+        return Files.list(Path.of(rootDirPath));
+    }
 }
