918823d57b7be10d71c0764471181b263e051622
h-c-b <hanna.baekkevold@gmail.com>
2024-03-15 05:40:44 +0000

    Clean up service and add validator method for path

--- src/main/java/com/example/demo/service/FileService.java
+++ src/main/java/com/example/demo/service/FileService.java
@@ -1,34 +1,42 @@
 package com.example.demo.service;
 
-import com.example.demo.repository.FileRepository;
+import com.example.demo.repository.IFileRepository;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
-public class FileService {
-    private final FileRepository _fileRepository;
+public class FileService implements IFileService {
+    private final IFileRepository fileRepository;
 
-    public FileService(FileRepository fileRepository) {
-        this._fileRepository = fileRepository;
+    public FileService(IFileRepository fileRepository) {
+        this.fileRepository = fileRepository;
     }
 
-    public void getFileNames() {
-        this._fileRepository.getFileNames();
-    }
-
-    public void uploadFile(MultipartFile file) {
-        this._fileRepository.writeFile(file);
-    }
-
-    public Resource downloadFile(String name) throws FileNotFoundException {
-        var fileToDownLoad = this._fileRepository.getFile(name);
-        if(fileToDownLoad.exists()) {
-            return fileToDownLoad;
-        } else {
-            throw new FileNotFoundException();
+    public List<String> getFileNames() throws IOException {
+        try (Stream<Path> fileStream = this.fileRepository.getFileNames()) {
+            return fileStream.map(Path::getFileName).map(Path::toString).collect(Collectors.toList());
         }
+    }
+
+    public boolean uploadFile(MultipartFile file) throws IOException {
+        return this.fileRepository.saveFile(file);
+    }
+
+    public Resource downloadFile(String fileName) throws MalformedURLException {
+        return this.fileRepository.getFile(fileName);
+    }
+
+    public void validateFileName(String fileName) throws InvalidPathException {
+        Paths.get(fileName);
     }
 }
